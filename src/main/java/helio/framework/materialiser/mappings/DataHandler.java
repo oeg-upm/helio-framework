e5d22007ad8b813de50df2b41e7afab161619d05
cimmino <cimmino@fi.upm.es>
2020-08-12 14:37:40 +0200

    data handlers updated

--- src/main/java/helio/framework/materialiser/mappings/DataHandler.java
+++ src/main/java/helio/framework/materialiser/mappings/DataHandler.java
@@ -4,9 +4,12 @@ import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Queue;
 
+import com.google.gson.JsonObject;
+
 public interface DataHandler extends Serializable{
 
 	public Queue<String> splitData(InputStream dataStream);
 	public String filter(String filter, String dataChunk); 
-	
+	public void configure(JsonObject configuration);
+
 }
