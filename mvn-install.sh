ba1ab75e405e792ed3ee34a69fba848d7d81f013
cimmino <cimmino@fi.upm.es>
2020-08-14 16:26:36 +0200

    version updated

--- mvn-install.sh
+++ mvn-install.sh
@@ -1,6 +1,6 @@
 # 1. Install the helio-framework dependency in your local repository using this script
 mvn clean install -DskipTests
-mvn install:install-file -Dfile=./target/framework-0.2.9.jar -DgroupId=upm.oeg.helio -DartifactId=framework -Dversion=0.2.9 -Dpackaging=jar
+mvn install:install-file -Dfile=./target/framework-0.3.0.jar -DgroupId=upm.oeg.helio -DartifactId=framework -Dversion=0.3.0 -Dpackaging=jar
 
 # 2. In the pom of your project import the follwing dependencies
 #
@@ -8,21 +8,8 @@ mvn install:install-file -Dfile=./target/framework-0.2.9.jar -DgroupId=upm.oeg.h
 #	  	<dependency>
 #			<groupId>oeg.helio.framework</groupId>
 #		   	<artifactId>helio-framework</artifactId>
-#		    <version>0.2.0</version>
+#		    <version>0.3.0</version>
 #		</dependency>
 #		<!-- Jena basic libraries (all in one): https://mvnrepository.com/artifact/org.apache.jena/apache-jena-libs -->
-#     	<dependency>
-#            <groupId>org.apache.jena</groupId>
-#            <artifactId>apache-jena-libs</artifactId>
-#            <version>3.6.0</version>
-#            <type>pom</type>
-#        </dependency>
-#    		<!-- CQEngine -->
-#		<dependency>
-#		    <groupId>com.googlecode.cqengine</groupId>
-#		    <artifactId>cqengine</artifactId>
-#		    <version>2.12.6</version>
-#		</dependency>
-
 
 # 3. You are ready to go and use the framework 
--- src/main/java/helio/framework/materialiser/mappings/DataHandler.java
+++ src/main/java/helio/framework/materialiser/mappings/DataHandler.java
@@ -2,6 +2,7 @@ package helio.framework.materialiser.mappings;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Queue;
 
 import com.google.gson.JsonObject;
@@ -9,7 +10,7 @@ import com.google.gson.JsonObject;
 public interface DataHandler extends Serializable{
 
 	public Queue<String> splitData(InputStream dataStream);
-	public String filter(String filter, String dataChunk); 
+	public List<String> filter(String filter, String dataChunk); 
 	public void configure(JsonObject configuration);
 
 }
